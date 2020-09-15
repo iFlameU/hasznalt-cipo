210e372d64f7d0d642f3a906ee471e919790c5e1
Norbert Racz <Norbert.Racz@partner.bmw.de>
2020-09-15 14:04:48 +0200

    Fixed logo font

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,14 @@ import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
 import {LoginComponent} from './login/login.component';
 import {MainComponent} from './main/main.component';
+import {RegisterComponent} from './register/register.component';
 
 
 const routes: Routes = [
+  {
+    path: 'register',
+    component: RegisterComponent
+  },
   {
     path: 'login',
     component: LoginComponent
